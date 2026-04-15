ba5804626cc93fae918d44198836004554ab1d86
Krish Patel <W0851433@myscc.ca>
2026-04-15 15:04:30 -0400

    Update db.js

--- no_lost_more_backend/db.js
+++ no_lost_more_backend/db.js
@@ -1,17 +1,14 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-// Test connection
 pool.connect()
-  .then(() => console.log("✅ PostgreSQL Connected"))
+  .then(() => console.log("✅ DB Connected"))
   .catch(err => console.error("❌ DB Connection Error FULL:", err));
 
 module.exports = pool;
